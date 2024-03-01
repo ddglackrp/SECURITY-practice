eeb06843d2d2b3dd4a77368a06988e3fb335a9ac
ddglackrp <ddglackrp@naver.com>
2024-03-01 20:09:49 +0900

    Docs "SecurityConfig 클래스 수정"
    스프링 시큐리티 버전 변경에 의한 Configuration 변경

--- src/main/java/hello/security/config/SecurityConfig.java
+++ src/main/java/hello/security/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity //스프링 시큐리티 활성화
+@Configuration
 public class SecurityConfig {
 
     @Bean
@@ -18,16 +19,20 @@ public class SecurityConfig {
     @Bean
     // 스프링 시큐리티 필터가 스프링 필터체인에 등록이 된다.
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-        http.authorizeRequests()
-                .antMatchers("/user/**").authenticated()
-                .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
-                .anyRequest().permitAll()
-                .and()
-                .formLogin()
-                .loginPage("/loginForm")
-                .loginProcessingUrl("/login")
-                .defaultSuccessUrl("/");
+        http.csrf(cs -> cs.disable());
+
+        http.authorizeHttpRequests(authorize ->
+                authorize
+                        .requestMatchers("/user/**").permitAll()
+                        .requestMatchers("/admin/**").hasAnyRole("hasRole('ROLE_ADMIN')")
+                        .anyRequest().permitAll()
+        );
+        http.formLogin(f ->
+                f.loginPage("/loginForm")
+                        .loginProcessingUrl("/login")
+                        .defaultSuccessUrl("/")
+        );
+
 
         return http.build();
     }
